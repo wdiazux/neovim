9bb6f748753589a9450599238f518bacedc65b77
William Diaz <root@wdiaz.org>
2024-07-13 10:25:29 -0600

    use nixfmt-rfc-style for nixd format

--- modules/nixvim/lsp/default.nix
+++ modules/nixvim/lsp/default.nix
@@ -131,23 +131,15 @@ in
         enable = true;
 
         settings = {
-          nixpkgs = {
-            expr = "import <nixpkgs> {}";
-          };
-          formatting = {
-            command = [ "nixpkgs-fmt" ];
-          };
+          nixpkgs.expr = "import <nixpkgs> {}";
+          formatting.command = [ "nixfmt" ];
           options = {
-            nixos = {
-              expr = ''
-                let configs = (builtins.getFlake ("git+file://" + builtins.toString ./.)).nixosConfigurations; in (builtins.head (builtins.attrValues configs)).options
-              '';
-            };
-            home_manager = {
-              expr = ''
-                let configs = (builtins.getFlake ("git+file://" + builtins.toString ./.)).homeConfigurations; in (builtins.head (builtins.attrValues configs)).options
-              '';
-            };
+            nixos.expr = ''
+              let configs = (builtins.getFlake ("git+file://" + builtins.toString ./.)).nixosConfigurations; in (builtins.head (builtins.attrValues configs)).options
+            '';
+            home_manager.expr = ''
+              let configs = (builtins.getFlake ("git+file://" + builtins.toString ./.)).homeConfigurations; in (builtins.head (builtins.attrValues configs)).options
+            '';
           };
         };
       };
