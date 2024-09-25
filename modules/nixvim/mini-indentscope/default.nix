265f71b50f0ee521b517b3fbe2039594ed4716f8
William Diaz <root@wdiaz.org>
2024-09-24 21:48:10 -0600

    fix autocmd condition

--- modules/nixvim/mini-indentscope/default.nix
+++ modules/nixvim/mini-indentscope/default.nix
@@ -1,26 +1,29 @@
 { config, lib, ... }:
 {
-  autoCmd = lib.mkIf (lib.hasAttr "indentscope" config.plugins.mini.modules) [
-    {
-      event = [ "FileType" ];
-      pattern = [
-        "help"
-        "alpha"
-        "ministarter"
-        "neo-tree"
-        "Trouble"
-        "trouble"
-        "mason"
-        "notify"
-        "toggleterm"
+  autoCmd =
+    lib.mkIf (config.plugins.mini.enable && lib.hasAttr "indentscope" config.plugins.mini.modules)
+      [
+        {
+          event = [ "FileType" ];
+          pattern = [
+            "help"
+            "alpha"
+            "dashboard"
+            "ministarter"
+            "neo-tree"
+            "Trouble"
+            "trouble"
+            "mason"
+            "notify"
+            "toggleterm"
+          ];
+          callback.__raw = ''
+            function()
+              vim.b.miniindentscope_disable = true
+            end
+          '';
+        }
       ];
-      callback.__raw = ''
-        function()
-          vim.b.miniindentscope_disable = true
-        end
-      '';
-    }
-  ];
   plugins.mini = {
     enable = true;
     modules.indentscope = { };
