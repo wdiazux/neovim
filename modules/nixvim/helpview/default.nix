d9e02a4c317d6b215d2be73c51f2b69b6076c52d
William Diaz <root@wdiaz.org>
2024-09-28 22:50:43 -0600

    helpview added

---
+++ modules/nixvim/helpview/default.nix
@@ -0,0 +1,20 @@
+{
+  plugins.helpview = {
+    enable = true;
+    settings = {
+      buf_ignore = [ ];
+
+      mode = [
+        "n"
+        "x"
+        "i"
+        "r"
+      ];
+
+      hybrid_modes = [
+        "i"
+        "r"
+      ];
+    };
+  };
+}
