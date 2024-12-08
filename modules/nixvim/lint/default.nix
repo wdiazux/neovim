00e894e5b36a27203aea0e2768cfbd586d1f8ac0
William Diaz <root@wdiaz.org>
2024-12-08 08:32:56 -0600

    lint added back

---
+++ modules/nixvim/lint/default.nix
@@ -0,0 +1,91 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  plugins = {
+    lint = {
+      enable = true;
+      lintersByFt = {
+        bash = [ "shellcheck" ];
+        css = lib.mkIf (!config.plugins.lsp.servers.stylelint_lsp.enable) [ "stylelint" ];
+        fish = [ "fish" ];
+        go = [ "golangcilint" ];
+        html = [ "htmlhint" ];
+        java = [ "checkstyle" ];
+        javascript = lib.mkIf (!config.plugins.lsp.servers.biome.enable) [ "biomejs" ];
+        json = [ "jsonlint" ];
+        lua = [ "luacheck" ];
+        make = [ "checkmake" ];
+        markdown = [ "markdownlint" ];
+        nix = [
+          "deadnix"
+          "nix"
+        ] ++ lib.optionals (!config.plugins.lsp.servers.statix.enable) [ "statix" ];
+        # FIXME: causes ruff errors
+        # python = [ "pylint" ];
+        rust = [ "clippy" ];
+        sh = [ "shellcheck" ];
+        sql = [ "sqlfluff" ];
+        typescript = lib.mkIf (!config.plugins.lsp.servers.biome.enable) [ "biomejs" ];
+        yaml = [ "yamllint" ];
+      };
+
+      linters = {
+        biomejs = {
+          cmd = lib.getExe pkgs.biome;
+        };
+        checkmake = {
+          cmd = lib.getExe pkgs.checkmake;
+        };
+        checkstyle = {
+          cmd = lib.getExe pkgs.checkstyle;
+        };
+        clippy = {
+          cmd = lib.getExe pkgs.rust-analyzer;
+        };
+        deadnix = {
+          cmd = lib.getExe pkgs.deadnix;
+        };
+        fish = {
+          cmd = lib.getExe pkgs.fish;
+        };
+        golangcilint = {
+          cmd = lib.getExe pkgs.golangci-lint;
+        };
+        htmlhint = {
+          cmd = lib.getExe pkgs.htmlhint;
+        };
+        jsonlint = {
+          cmd = lib.getExe pkgs.nodePackages.jsonlint;
+        };
+        luacheck = {
+          cmd = lib.getExe pkgs.luaPackages.luacheck;
+        };
+        markdownlint = {
+          cmd = lib.getExe pkgs.markdownlint-cli;
+        };
+        pylint = {
+          cmd = lib.getExe pkgs.pylint;
+        };
+        shellcheck = {
+          cmd = lib.getExe pkgs.shellcheck;
+        };
+        sqlfluff = {
+          cmd = lib.getExe pkgs.sqlfluff;
+        };
+        statix = {
+          cmd = lib.getExe pkgs.statix;
+        };
+        stylelint = {
+          cmd = lib.getExe pkgs.stylelint;
+        };
+        yamllint = {
+          cmd = lib.getExe pkgs.yamllint;
+        };
+      };
+    };
+  };
+}
