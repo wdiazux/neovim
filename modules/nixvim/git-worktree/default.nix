6454b0d66fe98487e76d131d8e02b4ecbf088f1f
William Diaz <root@wdiaz.org>
2025-01-25 07:33:04 -0600

    tweak keymaps

--- modules/nixvim/git-worktree/default.nix
+++ modules/nixvim/git-worktree/default.nix
@@ -28,22 +28,20 @@ in
     {
       mode = "n";
       key = "<leader>gWc";
-      action.__raw = ''
-        function()
-          require('telescope').extensions.git_worktree.create_git_worktree()
-        end
-      '';
-      options.desc = "Create worktree";
+      action = "<cmd>Telescope git_worktree create_git_worktree<CR>";
+      options = {
+        desc = "Create worktree";
+        silent = true;
+      };
     }
     {
       mode = "n";
       key = "<leader>gWs";
-      action.__raw = ''
-        function()
-          require('telescope').extensions.git_worktree.git_worktrees()
-        end
-      '';
-      options.desc = "Switch / Delete worktree";
+      action = "<cmd>Telescope git_worktree git_worktrees<CR>";
+      options = {
+        desc = "Switch / Delete worktree";
+        silent = true;
+      };
     }
   ];
 }
