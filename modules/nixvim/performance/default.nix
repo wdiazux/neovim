84e7b81a8e98befaf146671d752f1db8e143e119
William Diaz <root@wdiaz.org>
2025-01-29 01:26:38 -0600

    removed unused code

--- modules/nixvim/performance/default.nix
+++ modules/nixvim/performance/default.nix
@@ -1,12 +1,3 @@
-{ config, lib, ... }:
-let
-  snacksPickerEnabled =
-    config.plugins.snacks.enable
-    && lib.hasAttr "profiler" config.plugins.snacks.settings
-    && config.plugins.snacks.settings.profiler.enabled;
-
-  lazyEnabled = config.plugins.lz-n.enable || config.plugins.lazy.enable;
-in
 {
   performance.byteCompileLua = {
     enable = true;
